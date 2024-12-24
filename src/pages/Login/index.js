ed4935d989b376a39110959bd716b7cc5eb20b80
Rawlinson Rocha <rawlinson.95@gmail.com>
2024-12-23 21:49:26 -0300

    feature/login funcional

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { isEmail } from 'validator';
+import { useDispatch } from 'react-redux';
+import { get } from 'lodash';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import { toast } from 'react-toastify';
+import * as actions from '../../store/modules/auth/actions';
 
 //eslint-disable-next-line
-export default function Login() {
+export default function Login(props) {
+  const dispatch = useDispatch();
+
+  const prevPath = get(props, 'location.state.prevPath', '/');
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-mail inválido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha inválida');
+    }
+
+    if (formErrors) return;
+
+    dispatch(actions.loginRequest({ email, password, prevPath }));
+  };
+
   return (
     <Container>
       <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu e-mail"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
--- src/routes/MyRoute.js
+++ src/routes/MyRoute.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
-export default function MyRoute({
-  component: Component,
-  isClosed = false,
-  ...rest
-}) {
-  const isLoggedIn = false;
+export default function MyRoute({ component: Component, isClosed, ...rest }) {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
   if (isClosed && !isLoggedIn) {
     return (
