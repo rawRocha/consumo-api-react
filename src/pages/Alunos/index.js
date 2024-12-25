f12f6bc55441be0a63881e16b70e4cacd3a7e2cd
Rawlinson Rocha <rawlinson.95@gmail.com>
2024-12-25 18:49:03 -0300

    Loading funcionado.

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -7,20 +7,26 @@ import { Container } from '../../styles/GlobalStyles';
 import { AlunoContainer, ProfilePicture } from './styled';
 import axios from '../../services/axios';
 
+import Loading from '../../components/Loading';
+
 //eslint-disable-next-line
 export default function Alunos() {
   const [alunos, setAlunos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getData() {
+      setIsLoading(true);
       const response = await axios.get('/alunos');
       setAlunos(response.data);
+      setIsLoading(false);
     }
 
     getData();
   }, []);
   return (
     <Container>
+      <Loading isLoading={isLoading} />
       <h1>Alunos</h1>
 
       <AlunoContainer>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -4,6 +4,7 @@ import { isEmail } from 'validator';
 import { get } from 'lodash';
 
 import { Container } from '../../styles/GlobalStyles';
+import Loading from '../../components/Loading';
 import { Form } from './styled';
 import axios from '../../services/axios';
 import history from '../../services/history';
@@ -13,6 +14,7 @@ export default function Register() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -35,6 +37,8 @@ export default function Register() {
 
     if (formErrors) return;
 
+    setIsLoading(true);
+
     try {
       await axios.post('/users/', {
         nome,
@@ -48,11 +52,14 @@ export default function Register() {
       const errors = get(e, 'response.data.errors', []);
 
       errors.map((error) => toast.error(error));
+    } finally {
+      setIsLoading(false);
     }
   }
 
   return (
     <Container>
+      <Loading isLoading={isLoading} />
       <h1>Crie sua conta</h1>
 
       <Form onSubmit={handleSubmit}>
